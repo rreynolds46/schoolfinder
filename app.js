a89e6721c5f4ad60db84147400348b02f646d90e
Arkos <reynro03@gmail.com>
2018-07-10 07:19:47 -0400

    test

--- app.js
+++ app.js
@@ -66,3 +66,16 @@ if (app.get('env') === 'development') {
 }
 
 module.exports = app;
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('schoolfinder/build'));
+
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000; 
+
+app.listen(port, () => console.log(`Server on ${port}`));
+
